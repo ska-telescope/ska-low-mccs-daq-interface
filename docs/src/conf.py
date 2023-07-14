daf895b652280c4af8e889bcdfe233f24a3d330d
Tom Moynihan <tdm@observatorysciences.co.uk>
2023-07-14 15:23:19 +0000

    [MCCS-1633] favicon_mono -> favicon

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -34,7 +34,7 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 
 html_context = {
-    "favicon_url": "img/favicon_mono.ico",
+    "favicon_url": "img/favicon.ico",
     "logo_url": "img/logo.png",
     "theme_logo_only": True,
 }
