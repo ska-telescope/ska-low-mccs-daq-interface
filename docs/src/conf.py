198c41d1b5847cdb7552b534a99add0b45d6f3a5
Elliot Scott <ejs@observatorysciences.co.uk>
2023-08-04 09:47:46 +0100

    [REL-831] Prepare to release v0.1.1

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -15,7 +15,7 @@ sys.path.insert(0, os.path.abspath("../../src"))
 project = 'ska-low-mccs-daq-interface'
 copyright = "2023, SKAO"
 author = "MCCS Team <drew.devereux@skao.int>"
-release = '0.1.0'
+release = '0.1.1'
 
 extensions = [
     "sphinx.ext.autodoc",
